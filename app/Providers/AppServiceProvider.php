d2775c90275eb7b6526b42fb22651e632adf86de
Wasim Ansari <matainja0120@gmail.com>
2025-02-23 13:17:35 +0530

    change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Vite;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
